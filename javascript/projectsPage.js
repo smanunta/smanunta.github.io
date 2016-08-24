626a358412c76dc8f6258d132dcf3fb0fa439aa8
jose sebastian manunta <smanunta@gmail.com>
2016-08-24 00:21:49 -0400

    updated the projects design

--- javascript/projectsPage.js
+++ javascript/projectsPage.js
@@ -1,4 +1,8 @@
 $(document).ready(function()
 {
-    
+    // side menu buttons hiding func
+    $(".sideMenuItem").click(function()
+    {
+      $(".wrapper-section section.collapse").collapse("hide");
+    });
 });
