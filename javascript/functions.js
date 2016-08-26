bbaf974a54d1a9236e8205205ec6d70ec4dfbf18
jose sebastian manunta <smanunta@gmail.com>
2016-08-26 00:09:02 -0400

    finished the projects section and updated index btn

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,9 +1,14 @@
 $(document).ready(function()
 {
-	$("#small-title").click(function()
+	$("#aboutMeBtn").click(function()
 {
     window.location = "aboutMe.html";
 });
+$("#projectsBtn").click(function()
+{
+	window.location = "projects.html";
+});
+
 	$('#header_and_wrapper_footer #animated_footer li a').hover(function()
 	{
 		$('#header_and_wrapper_footer #animated_footer li a').addClass('animated infinite swing').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
